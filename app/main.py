e0db3c5e96b1c65a789504a7be44306ed191e9ae
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-07-20 23:36:08 +1200

    limiter.slowapi

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from app.routers import chat
 
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from app.limiter import limiter
+
+
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 app.include_router(chat.router)
 
 @app.get("/")
